d5d106d0e9ee001c0a8e45b0f940615258db6905
Fattaneh2016 <fatameri@hotmail.com>
2016-07-09 18:43:16 -0400

    add strokeStyle

--- Script.js
+++ Script.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
     //drawPattern();
     //drawImage();
     //drawLineWidth();
-    drawLineJoin();
+    //drawLineJoin();
+    drawGradientStroke();
 });
 
 
@@ -203,4 +204,53 @@ function drawSomething()
 
         ctx.lineJoin = 'miter';
         ctx.strokeRect(180, 180, 50, 50);
+    }
+
+    function drawGradientStroke() {
+        var canvas = document.getElementById('myCanvas')
+            , ctx = canvas.getContext('2d')
+            , x0 = 0
+            , y0 = 0
+            , r0 = 0
+            , x1 = 200
+            , y1 = 0
+            , r1 = 100
+            , width = 300
+            , height = 40
+            , offset = 25;
+
+        ctx.lineWidth = 15;
+        gradient = ctx.createLinearGradient(x0, y0, x1, y1);
+        addColorStops(gradient);
+        ctx.strokeStyle = gradient;
+        ctx.strokeRect(10, 0 * (height + offset), width, height);
+        ctx.strokeRect(100, 1 * (height + offset), width, height);
+        y1 = 300;
+        gradient = ctx.createLinearGradient(x0, y0, x1, y1);
+        addColorStops(gradient);
+        ctx.strokeStyle = gradient;
+        ctx.strokeRect(10, 2 * (height + offset), width, height);
+        ctx.strokeRect(100, 3 * (height + offset), width, height);
+
+        x0 = x1 = width / 2;
+        y0 = y1 = 4 * (height + offset) + (height / 2);
+        gradient = ctx.createRadialGradient(x0, y0, r0, x1, y1, r1);
+        addColorStops(gradient);
+        ctx.strokeStyle = gradient;
+        ctx.strokeRect(10, 4 * (height + offset), width, height);
+        ctx.strokeRect(100, 5 * (height + offset), width, height);
+        y0 = 5 * (height + offset) + (height / 2);
+        y1 = y0 + 100;
+        gradient = ctx.createRadialGradient(x0, y0, r0, x1, y1, r1);
+        addColorStops(gradient);
+        ctx.strokeStyle = gradient;
+        ctx.strokeRect(10, 6 * (height + offset), width, height);
+        ctx.strokeRect(100, 7 * (height + offset), width, height);
+    }
+    function addColorStops(gradient) {
+        gradient.addColorStop("0", "magenta");
+        gradient.addColorStop(".25", "blue");
+        gradient.addColorStop(".50", "green");
+        gradient.addColorStop(".75", "yellow");
+        gradient.addColorStop("1.0", "red");
     }
