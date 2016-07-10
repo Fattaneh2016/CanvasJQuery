2adf4ff80e407ce08c28cec7204e206eca92842e
Fattaneh2016 <fatameri@hotmail.com>
2016-07-10 00:09:55 -0400

    add beginpath

--- Script.js
+++ Script.js
@@ -8,7 +8,8 @@ $(document).ready(function() {
     //drawLineWidth();
     //drawLineJoin();
     //drawGradientStroke();
-    saveRestore();
+    //saveRestore();
+    drawLine();
 });
 
 
@@ -273,4 +274,44 @@ function drawSomething()
 
         ctx.restore();
         ctx.strokeRect(180, 180, 50, 50);
+    }
+
+    function drawLine(){
+        var canvas = document.getElementById('myCanvas')
+        , ctx = canvas.getContext('2d');
+
+        ctx.fillStyle = 'green';
+        ctx.strokeStyle = 'yellow';
+        ctx.lineWidth = 10;
+
+        ctx.beginPath();
+        ctx.moveTo(100, 250);
+        ctx.lineTo(150, 350);
+        ctx.lineTo( 50, 350); 
+        ctx.closePath(); 
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.moveTo(150, 250);
+        ctx.lineTo(250, 250);
+        ctx.lineTo(200, 350); 
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(300, 250);
+        ctx.lineTo(350, 350);
+        ctx.lineTo(250, 350);
+        ctx.fill();
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(500, 250);
+        ctx.lineTo(500, 350);
+        ctx.moveTo(450, 300);
+        ctx.lineTo(550, 300)
+        ctx.fill();
+        ctx.stroke();
+
     }
